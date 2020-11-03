ae8c26f1e22178345ef5cf4f1330354e777e4866
Constantin Amundsen <constantin.amundsen@home24.de>
2020-11-03 12:28:52 +0100

    added more comments and maybe a fix for the array out of index error when not adding separator lines

--- menustyling/coreFunctions.go
+++ menustyling/coreFunctions.go
@@ -39,37 +39,55 @@ func (menu *Menu) getHeight() {
 	menu.height = (height + (menu.lineThickness * 2)) + sepHeight
 }
 
+// Function to put Whitespaces, Separator lines and the Menu text into the Menu field
 func (menu *Menu) putText() {
 
-	indexOfTextLine := 0
-	indexOfTextChar := 0
+	// Set some local variables to keep track of important positions and configurations.
+	// Keep track of the current Textblock index
 	indexOfTextBlock := 0
+	// Keep track of the current Text Line of the current text block.
+	indexOfTextLine := 0
+	// Keep track of the current Character of the current text line.
+	indexOfTextChar := 0
+	// Mod value to insert the Separator correctly
 	indexModForSeparator := 0
+	// The index of text line between to text blocks where the line is placed.
 	indexOfSeparatorLine := 0
 
+	// Set first line at the line thickness value, as we dont need to change anything before that.
 	for indexOfMenuLine := menu.lineThickness; indexOfMenuLine < menu.height - menu.lineThickness; indexOfMenuLine++ {
+		// At the beginning off each new text line, set the character index back to 0.
 		indexOfTextChar = 0
+		// Insert the Text inside the MenuText array into the Menu field.
 		for indexOfMenuChar := menu.lineThickness*2 + menu.whiteSpace; indexOfMenuChar < len(menu.menuText[indexOfTextBlock][indexOfTextLine]) + menu.lineThickness*2 + menu.whiteSpace; indexOfMenuChar++ {
 			menu.menuArray[indexOfMenuLine][indexOfMenuChar] = string(menu.menuText[indexOfTextBlock][indexOfTextLine][indexOfTextChar])
 			indexOfTextChar++
 		}
 
+		// If some number of Whitespace is specified, replace the space before and after the text with spaces.
 		if menu.whiteSpace != 0 {
+			// This replaces characters before the text.
 			for indexOfWhiteSpace := menu.lineThickness*2 + menu.whiteSpace - 1; indexOfWhiteSpace > menu.lineThickness*2 - 1; indexOfWhiteSpace-- {
 				menu.menuArray[indexOfMenuLine][indexOfWhiteSpace] = " "
 			}
+			// this replaces characters after the text.
 			for indexOfWhiteSpace := menu.lineThickness*2 + menu.whiteSpace + len(menu.menuText[indexOfTextBlock][indexOfTextLine]); indexOfWhiteSpace < menu.width - menu.lineThickness*2; indexOfWhiteSpace++ {
 				menu.menuArray[indexOfMenuLine][indexOfWhiteSpace] = " "
 			}
 		}
 
+		// At the end of the loop, add 1 to the textline index and separator line index.
 		indexOfSeparatorLine++
 		indexOfTextLine++
+		// if the separator line option is true, hop over one text line to leave a blank separator line.
 		if menu.separatorLines && indexModForSeparator != len(menu.menuText) - 1 && indexOfSeparatorLine == len(menu.menuText[indexOfTextBlock]) {
 			indexOfMenuLine++
 			indexModForSeparator++
 			indexOfSeparatorLine = 0
 			indexOfTextBlock++
+			indexOfTextLine = 0
+		} else if !menu.separatorLines {
+			indexOfSeparatorLine = 0
 			indexOfTextLine = 0
 		}
 	}
